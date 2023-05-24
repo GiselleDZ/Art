6c2d31fda5d187cfae2e17e99d9c3b985d96a158
GiselleDZ <giselle@gisellezatonyl.com>
2023-05-24 19:59:41 -0400

    feat: project photos for modular landscapes

--- src/Components/Projects/ProjectData/modularLandscapes.ts
+++ src/Components/Projects/ProjectData/modularLandscapes.ts
@@ -1,13 +1,18 @@
 const modularLandscapesData = {
   title: "Modular Landscapes",
   coverImage:
-    "https://res.cloudinary.com/delu9m4xu/image/upload/v1684964341/Art-Site/Modular-Landscapes/12028687_10208018435278202_8949771425615476840_o-u9305-fr_tczhlv.png",
+    "https://res.cloudinary.com/delu9m4xu/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1684964341/Art-Site/Modular-Landscapes/12028687_10208018435278202_8949771425615476840_o-u9305-fr_tczhlv.png",
   description: "A series of ship windows traveling to an array of landscapes.",
   year: 2016,
   projectPhotos: [
+    "https://res.cloudinary.com/delu9m4xu/image/upload/v1684964341/Art-Site/Modular-Landscapes/12028687_10208018435278202_8949771425615476840_o-u9305-fr_tczhlv.png",
     "https://res.cloudinary.com/delu9m4xu/image/upload/v1684540837/Art-Site/Modular-Landscapes/ggrphcllyindtrmntfntss-modulardoc-03_kvwl8l.jpg",
   ],
-  installPhotos: [],
+  installPhotos: [
+    "https://res.cloudinary.com/delu9m4xu/image/upload/v1684972706/Art-Site/Modular-Landscapes/ggrphcllyindtrmntfntss-modulardoc-03-full_mbqsam.jpg",
+    "https://res.cloudinary.com/delu9m4xu/image/upload/v1684540838/Art-Site/Modular-Landscapes/ggrphcllyindtrmntfntss-modulardoc-02_gltczn.jpg",
+    "https://res.cloudinary.com/delu9m4xu/image/upload/v1684540838/Art-Site/Modular-Landscapes/ggrphcllyindtrmntfntss-modulardoc-01_ac5tz8.jpg",
+  ],
 };
 
 export default modularLandscapesData;
