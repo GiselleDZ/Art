b6f74c579f5add6829005924e1c1c2211ec3c039
GiselleDZ <giselle@gisellezatonyl.com>
2023-05-24 20:07:37 -0400

    fix: export from template files

---
+++ src/Components/Projects/ProjectData/tundra.ts
@@ -0,0 +1,12 @@
+const alienShipData = {
+  title: "Alien Ship",
+  coverImage:
+    "https://res.cloudinary.com/delu9m4xu/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1684964506/Art-Site/AlienShip/alienship_09_03_15.jpg",
+  description:
+    "A tongue in cheek self-portrait depicting an alien ship transporting away the artist as alien. The work was created as a short gif and an 20x30in aluminum image transfer.",
+  year: 2015,
+  projectPhotos: [],
+  installPhotos: [],
+};
+
+export default alienShipData;
